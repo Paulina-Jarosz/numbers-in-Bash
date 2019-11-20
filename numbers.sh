a641cd51e31208dbd14f9faa5c9f1dc9a0dc437d
Paulina Jarosz <paulinajarosz23@gmail.com>
2019-11-20 16:04:30 +0100

    basic examples 2

---
+++ numbers.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#hackerrank exercise
+
+x=50
+y=10
+
+a=$(( $x + $y ))
+b=$(( $x - $y ))
+c=$(( $x * $y ))
+d=$(( $x / $y ))
+
+echo $a
+echo $b
+echo $c
+echo $d
+
+
+read x
+read y
+
